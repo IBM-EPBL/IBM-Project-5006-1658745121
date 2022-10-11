0e9790f6fd07a1a48c372f9bf228691d23c3a4f7
Janeshwar S <68996644+Janesh-TS@users.noreply.github.com>
2022-10-11 21:12:20 +0530

    Create Code.py

---
+++ Assignments/Assignment 2/Annamalai K/Code.py
@@ -0,0 +1,17 @@
+import random
+temperature=(random.random () ) *100
+Humidity= (random. random () ) *100
+roundedTemp=round (temperature)
+roundedHumi=round (Humidity)
+print ("The temperature is", roundedTemp)
+if roundedTemp>50:
+    print ("temperature is high")
+else:
+    print ("temperature is low")
+temperature= (random.random () ) *100
+undedTemp=round (temperature)
+print ("The Humidity is", roundedHumi)
+if roundedHumi>50:
+    print ("Humidity is high")
+else:
+    print ("Humidity is low")
